e42ff95607a044135f2028506cf1f142f9bf6d5d
Rakkan Tiasakul <rakkant@Rakkans-MacBook-Pro.local>
2017-05-26 21:15:07 +0700

    remove unused classes

--- src/network/Network.java
+++ src/network/Network.java
@@ -5,11 +5,10 @@ import com.esotericsoftware.kryonet.EndPoint;
 
 import game.Board;
 import game.Boat;
-import game.Bullet;
-import game.Field;
+
 import game.Game;
 import game.GameLogic;
-import game.Player;
+
 import game.Ship;
 
 
@@ -21,10 +20,8 @@ public static final int CONNECT = 1;
 		Kryo k = endpoint.getKryo();
 		//  need to register all classes that we want to send over network
 		k.register(Game.class);
-		k.register(Player.class);
-		k.register(Field.class);
+
 		k.register(Ship.class);
-		k.register(Bullet.class);
 		k.register(Board.class);
 		k.register(GameLogic.class);
 		k.register(Boat.class);
